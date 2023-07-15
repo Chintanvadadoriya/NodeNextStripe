4bf256403cfdfe9af07cbc0ec2d2c5d907f650f0
chintan <chintan1612199@gmail.com>
2023-07-15 12:11:25 +0530

    express server setup

---
+++ Express-Server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express()
+const dotenv = require('dotenv')
+const userRoute = require('./routes/users')
+
+app.use(express.json())
+
+app.use('/api/users', userRoute)
+
+
+app.listen(5000,()=>{
+     console.log("server is running 5000 port");
+})    
